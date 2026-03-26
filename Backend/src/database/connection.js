47b49451743f731d7607e80d91eaa63daa978e2c
Wilian Zembrani <Wilian3355@gmail.com>
2026-03-26 15:57:32 -0300

    chore: configura conexão com banco de dados e servidor inicial

--- Backend/src/database/connection.js
+++ Backend/src/database/connection.js
@@ -1,1 +1,20 @@
-const mysql = require("mysql2");
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+connection.connect((error) => {
+  if (error) {
+    console.log("Erro ao conectar ao banco de dados");
+  } else {
+    console.log("Conectado com sucesso");
+  }
+});
+
+module.exports = connection;
